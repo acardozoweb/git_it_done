a0db908a3018d6b03ab56349a3fff8f9f09db0b1
anthonycc <anthonyccardozo@gmail.com>
2022-01-12 17:07:49 -0500

    homepage changes

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,22 @@
-var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
-    });
-  };
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 
-getUserRepos("bandcamp");
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  console.log(event);
+};
+
+var getUserRepos = function (user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+getUserRepos(acardozoweb);
